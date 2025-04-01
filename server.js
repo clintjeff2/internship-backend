b624bfc3efc27b97008666f03f78dcb1742d8a75
clintjeff2 <clintjeff2@gmail.com>
2025-04-01 15:26:48 +0100

    fixed the databas connectivity issue

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // handling uncaught exceptions- synchronous code
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXPRESSION! 🔴 Shutting down...");
+process.on('uncaughtException', (err) => {
+	console.log('UNCAUGHT EXPRESSION! 🔴 Shutting down...');
 
-  console.log(err.name, err.message);
-  process.exit(1);
+	console.log(err.name, err.message);
+	process.exit(1);
 });
 
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
-// const DB = "mongodb://127.0.0.1:27017/internship";
 const DB =
-  "mongodb+srv://vanessafuangi:gO8QLhdUz6CWAQMA@internship.wh3lyvn.mongodb.net/?retryWrites=true&w=majority&appName=Internship";
+	'mongodb+srv://clintjeff2:kljlsljLKJKLDJOIj12lj)_@cluster0.aifxmvj.mongodb.net/internship?retryWrites=true&w=majority';
 
 mongoose.connect(DB).then((con) => {
-  console.log("DB connection successful");
+	console.log('DB connection successful');
 });
 
 // Starting the server
 const port = 4000;
 
 app.listen(port, () => {
-  console.log(`The app is running on port ${port}...`);
+	console.log(`The app is running on port ${port}...`);
 });
 
 // handling unhandled rejected promises - asynchronous code
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 🔴 Shutting down...");
+process.on('unhandledRejection', (err) => {
+	console.log('UNHANDLED REJECTION! 🔴 Shutting down...');
 
-  console.log(err.name, err.message);
-  // server.close(() => {
-  process.exit(1);
-  // });
+	console.log(err.name, err.message);
+	// server.close(() => {
+	process.exit(1);
+	// });
 });
