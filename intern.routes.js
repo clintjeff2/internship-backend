9d739b76c3a9cd12dcea42a8718c791f5c333567
clintjeff2 <clintjeff2@gmail.com>
2025-04-29 11:48:49 +0100

    Added route to download file controller

--- intern.routes.js
+++ intern.routes.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const internController = require("./internController");
+const express = require('express');
+const internController = require('./internController');
 
 const router = express.Router();
 
 router
-  .route("/")
-  .get(internController.getAllInterns)
-  .post(internController.registerIntern);
+	.route('/')
+	.get(internController.getAllInterns)
+	.post(internController.registerIntern);
 
 router
-  .route("/:id")
-  .get(internController.getIntern)
-  .patch(internController.updateIntern)
-  .delete(internController.deleteIntern);
+	.route('/:id')
+	.get(internController.getIntern)
+	.patch(internController.updateIntern)
+	.delete(internController.deleteIntern);
+
+router.get('/excel/file', internController.getExcelFile);
 
 module.exports = router;
